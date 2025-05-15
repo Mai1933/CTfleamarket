b8d08d740a4a8b99a6d68752dd650f8fcb3f85a3
mai fujimoto <MaiFujimoto1933@outlook.jp>
2025-05-15 14:32:52 +0900

    ブランチのテスト

---
+++ src/resources/views/chat.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts/header_search')
+
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/detail.css') }}">
+@endsection
+
+@section('content')
+    <main>
+    </main>
+@endsection
